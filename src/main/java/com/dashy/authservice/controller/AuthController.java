253bf5ceb01bdfa44733ef4fc173b0c31ec3ff12
AmineNaboulsim <naboulsiiamine@gmail.com>
2025-05-26 20:22:06 +0100

    chor: samll changes

--- src/main/java/com/dashy/authservice/controller/AuthController.java
+++ src/main/java/com/dashy/authservice/controller/AuthController.java
@@ -7,16 +7,22 @@ import com.dashy.authservice.exception.SuccessResponse;
 import com.dashy.authservice.model.User;
 import com.dashy.authservice.service.AuthService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthService authService;
+    private static final Logger logger = LoggerFactory.getLogger(User.class);
 
     public AuthController(AuthService authService) {
         this.authService = authService;
@@ -30,5 +36,20 @@ public class AuthController {
     @GetMapping("/test")
     public String Test() {
         return "test ";
+    }
+
+    @PostMapping("/send-otp-validation")
+    public ResponseEntity<?> SendOTPValidation(){
+        logger.info("\n /send-otp-validation was called \n");
+        return ResponseEntity.ok(
+                new SuccessResponse(
+                        "Verification OTP Sended", "Done"));
+    }
+    @PostMapping("/send-email-validation")
+    public ResponseEntity<?> SendEmailValidation(){
+        logger.info("\n /send-email-validation was called \n");
+        return ResponseEntity.ok(
+                new SuccessResponse(
+                        "Verification Mail Sended", "Done"));
     }
 }
